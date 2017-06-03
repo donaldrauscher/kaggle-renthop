0a4d3bbec1395ea9f7840a73f2e5d0b0e1c776e7
donaldrauscher <donald.rauscher@gmail.com>
2017-06-03 16:58:19 +0000

    Change how we apply high-card cat to test set in CV

--- universes/base.R
+++ universes/base.R
@@ -8,8 +8,8 @@ load("./data/extract_train.Rdata")
 load("./data/extract_test.Rdata")
 source("./setup/feature_functions.R")
 
-# set up datasets on all data
-add_high_card_cat <- function(df){
+# add high-cardinality categorical
+add_high_card_cat_train <- function(df){
   df$building_interest_h <- probabilize_high_card_cat(df, ifelse(interest_level == "high", 1, 0), building_id)
   df$building_interest_m <- probabilize_high_card_cat(df, ifelse(interest_level == "medium", 1, 0), building_id)
   df$manager_interest_h <- probabilize_high_card_cat(df, ifelse(interest_level == "high", 1, 0), manager_id)
@@ -19,16 +19,24 @@ add_high_card_cat <- function(df){
   return(df)
 }
 
-high_card_cat <- c("building_interest_h", "building_interest_m", "manager_interest_h", "manager_interest_m", "neighborhood_interest_h", "neighborhood_interest_m") 
-data_train_processed2 <- add_high_card_cat(data_train_processed)
-data_train_processed2 <- add_noise(data_train_processed2, high_card_cat)
-data_train_processed2 <- make_binary(data_train_processed2, grep("[k][0-9]{3}", names(data_train_processed2), value=TRUE), 0)
+add_high_card_cat_test <- function(df_train, df_test){
+  building_interest_var <- df_train %>% select(building_id, building_interest_h, building_interest_m) %>% distinct()
+  manager_interest_var <- df_train %>% select(manager_id, manager_interest_h, manager_interest_m) %>% distinct()
+  neighbor_interest_var <- df_train %>% select(neighborhood_id, neighborhood_interest_h, neighborhood_interest_m) %>% distinct()
+  df_test <- df_test %>% left_join(building_interest_var, by = c("building_id")) %>% left_join(manager_interest_var, by = c("manager_id")) %>% left_join(neighbor_interest_var, by = c("neighborhood_id"))
+  return(df_test)
+}
 
-data_test_processed <- data_test_processed %>% 
-  left_join(data_train_processed2[,c("building_id", "building_interest_h", "building_interest_m")], by = c("building_id")) %>%
-  left_join(data_train_processed2[,c("manager_id", "manager_interest_h", "manager_interest_m")], by = c("manager_id")) %>%
-  left_join(data_train_processed2[,c("neighborhood_id", "neighborhood_interest_h", "neighborhood_interest_m")], by = c("neighborhood_id"))
-data_test_processed2 <- impute(data_test_processed, high_card_cat, mean)
+data_train_processed2 <- add_high_card_cat_train(data_train_processed)
+data_test_processed2 <- add_high_card_cat_test(data_train_processed2, data_test_processed)
+
+# add noise to train / impute for test
+high_card_cat <- c("building_interest_h", "building_interest_m", "manager_interest_h", "manager_interest_m", "neighborhood_interest_h", "neighborhood_interest_m") 
+data_train_processed2 <- add_noise(data_train_processed2, high_card_cat)
+data_test_processed2 <- impute(data_test_processed2, high_card_cat, mean)
+
+# make some variables binar
+data_train_processed2 <- make_binary(data_train_processed2, grep("[k][0-9]{3}", names(data_train_processed2), value=TRUE), 0)
 data_test_processed2 <- make_binary(data_test_processed2, grep("[k][0-9]{3}", names(data_test_processed2), value=TRUE), 0)
 
 holdouts <- c(model_exclude_var, grep("[m,b][0-9]{3}", names(data_test_processed2), value=TRUE))
@@ -44,13 +52,19 @@ cv_data <- lapply(1:max(cv), function(i){
   train <- which(cv != i)
   test <- which(cv == i)
   
-  # set up train set
-  xdata_i_train <- add_high_card_cat(data_train_processed[train,])
+  # set up train and test sets
+  xdata_i_train <- add_high_card_cat_train(data_train_processed[train,])
+  xdata_i_test <- add_high_card_cat_test(xdata_i_train, data_train_processed[test,])
+  
   xdata_i_train <- add_noise(xdata_i_train, high_card_cat)
+  xdata_i_test <- impute(xdata_i_test, high_card_cat, mean)
+  
   xdata_i_train <- make_binary(xdata_i_train, grep("[k][0-9]{3}", names(xdata_i_train), value=TRUE), 0)
-
-  # set up test set; leave one out on test+train for cross validation test set
-  xdata_i_test <- xdata[test,]
+  xdata_i_test <- make_binary(xdata_i_test, grep("[k][0-9]{3}", names(xdata_i_test), value=TRUE), 0)
+  
+  xdata_i_train <- Matrix(as.matrix(xdata_i_train[,xvar]), sparse = TRUE)
+  xdata_i_test <- Matrix(as.matrix(xdata_i_test[,xvar]), sparse = TRUE)
+  #xdata_i_test <- xdata[test,] # if want to use 'interest' data in test set in computation of high-card cat
   
   # return
   return(list(train=train, test=test, xdata_train=xdata_i_train, xdata_test=xdata_i_test))
