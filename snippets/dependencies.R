87f21b3da8de643735655e21afcc80a3e780d419
donaldrauscher <donald.rauscher@gmail.com>
2017-06-22 03:40:45 +0000

    Make dependencies a little more general; general edits

--- snippets/dependencies.R
+++ snippets/dependencies.R
@@ -6,14 +6,15 @@ dep_outputs <- sapply(dep, function(d) get_step_output(args$pipeline, d))
 # load dependencies
 for (d in dep_outputs){
   load(sprintf("./data/models/%s.Rdata", d))
-  
+  var_list <- seq(ncol(as.data.frame(step_outputs$validate_predictions)))
+
   # clean up validate predictions
   step_outputs$validate_predictions <- as.data.frame(step_outputs$validate_predictions)
-  names(step_outputs$validate_predictions) <- paste(d, c("low", "medium", "high"), sep = "_")
+  names(step_outputs$validate_predictions) <- paste(d, var_list, sep = "_")
 
   # clean up test predictions
   step_outputs$test_predictions <- step_outputs$test_predictions %>% select(-listing_id)
-  names(step_outputs$test_predictions) <- paste(d, c("low", "medium", "high"), sep = "_")
+  names(step_outputs$test_predictions) <- paste(d, var_list, sep = "_")
 
   assign(d, step_outputs)
 }
--- universes/base.R
+++ universes/base.R
@@ -12,13 +12,23 @@ load("./data/extract_test.Rdata")
 source("./snippets/feature_functions.R")
 
 # pull in universe params and set defaults if missing
-if(is.null(univ_param$add_noise)) univ_param$add_noise <- 1
 if(is.null(univ_param$include_kw_var)) univ_param$include_kw_var <- 1
-if(is.null(univ_param$high_card_loo)) univ_param$high_card_loo <- 1
+if(is.null(univ_param$include_hcc)) univ_param$include_hcc <- 1
+if(is.null(univ_param$add_hcc_noise)) univ_param$add_hcc_noise <- 1
+if(is.null(univ_param$hcc_loo)) univ_param$hcc_loo <- 1
 
 # combine datasets
 data_all_processed <- bind_rows(data_train_processed, data_test_processed)
-if(length(dep_outputs)>0) data_all_processed <- cbind(data_all_processed,  bind_rows(dep_validate_predictions, dep_test_predictions))
+if(length(dep_outputs)>0){
+  all_pred <- bind_rows(dep_validate_predictions, dep_test_predictions)
+  if(!is.null(step_def$dependency_normalizer)){
+    dependency_normalizer <- step_def$dependency_normalizer
+    for (i in names(all_pred)){
+      all_pred[[i]] <- all_pred[[i]] / data_all_processed[[dependency_normalizer]]
+    }
+  }
+  data_all_processed <- cbind(data_all_processed, all_pred)
+}
 is_train <- which(!is.na(data_all_processed$interest_level))
 is_test <- inv_which(is_train, nrow(data_all_processed))
 
@@ -74,7 +84,7 @@ data_all_processed <- data_all_processed %>%
     building_mean_price_per_bedroom_rel = ifelse(building_mean_price_per_bedroom == 0, 1, price_per_bedroom / building_mean_price_per_bedroom),
     building_mean_price_per_room_rel = ifelse(building_mean_price_per_room == 0, 1, price_per_room / building_mean_price_per_room)
   ) %>% ungroup() %>%
-  group_by(neighborhood_id) %>%
+  group_by(neighborhood_id, bedrooms) %>%
   mutate(
     neighborhood_mean_price = mean(price),
     neighborhood_mean_bedroom = mean(bedrooms),
@@ -104,14 +114,14 @@ data_all_processed <- data_all_processed %>%
 # add high-cardinality categorical
 add_high_card_cat <- function(df){
   df <- df %>% group_by(building_id) %>% mutate(seg = cut(n(), breaks = c(0, 10, 30, Inf))) %>% ungroup()
-  df$building_interest_h <- probabilize_high_card_cat(df, ifelse(interest_level == "high", 1, 0), building_id, seg, univ_param$high_card_loo)
-  df$building_interest_m <- probabilize_high_card_cat(df, ifelse(interest_level == "medium", 1, 0), building_id, seg, univ_param$high_card_loo)
+  df$building_interest_h <- probabilize_high_card_cat(df, ifelse(interest_level == "high", 1, 0), building_id, seg, univ_param$hcc_loo)
+  df$building_interest_m <- probabilize_high_card_cat(df, ifelse(interest_level == "medium", 1, 0), building_id, seg, univ_param$hcc_loo)
   df <- df %>% group_by(manager_id) %>% mutate(seg = cut(n(), breaks = c(0, 10, 30, Inf))) %>% ungroup()
-  df$manager_interest_h <- probabilize_high_card_cat(df, ifelse(interest_level == "high", 1, 0), manager_id, seg, univ_param$high_card_loo)
-  df$manager_interest_m <- probabilize_high_card_cat(df, ifelse(interest_level == "medium", 1, 0), manager_id, seg, univ_param$high_card_loo)
-  df <- df %>% group_by(neighborhood_id) %>% mutate(seg = cut(n(), breaks = c(0, 10, 30, Inf))) %>% ungroup()
-  df$neighborhood_interest_h <- probabilize_high_card_cat(df, ifelse(interest_level == "high", 1, 0), neighborhood_id, seg, univ_param$high_card_loo)
-  df$neighborhood_interest_m <- probabilize_high_card_cat(df, ifelse(interest_level == "medium", 1, 0), neighborhood_id, seg, univ_param$high_card_loo)
+  df$manager_interest_h <- probabilize_high_card_cat(df, ifelse(interest_level == "high", 1, 0), manager_id, seg, univ_param$hcc_loo)
+  df$manager_interest_m <- probabilize_high_card_cat(df, ifelse(interest_level == "medium", 1, 0), manager_id, seg, univ_param$hcc_loo)
+  df <- df %>% mutate(seg = 1)
+  df$neighborhood_interest_h <- probabilize_high_card_cat(df, ifelse(interest_level == "high", 1, 0), neighborhood_id, seg, univ_param$hcc_loo)
+  df$neighborhood_interest_m <- probabilize_high_card_cat(df, ifelse(interest_level == "medium", 1, 0), neighborhood_id, seg, univ_param$hcc_loo)
   df <- df %>% select(-seg)
   return(df)
 }
@@ -120,7 +130,7 @@ data_all_processed2 <- add_high_card_cat(data_all_processed)
 
 # add noise to train
 high_card_cat <- c("building_interest_h", "building_interest_m", "manager_interest_h", "manager_interest_m", "neighborhood_interest_h", "neighborhood_interest_m") 
-if (univ_param$add_noise){
+if (univ_param$add_hcc_noise){
   data_all_processed2 <- add_noise(data_all_processed2, is_train, high_card_cat)
 }
 
@@ -129,10 +139,19 @@ kw_var <- grep("[k][0-9]{3}", names(data_all_processed2), value=TRUE)
 data_all_processed2 <- make_binary(data_all_processed2, kw_var, 0)
 
 # assemble x and y
-holdouts <- model_exclude_var
+holdouts <- setdiff(model_exclude_var, c("latitude", "longitude"))
+building_var <- grep("[b][0-9]{3}", names(data_all_processed2), value=TRUE)
+manager_var <- grep("[m][0-9]{3}", names(data_all_processed2), value=TRUE)
+neighborhood_var <- grep("[n][0-9]{3}", names(data_all_processed2), value=TRUE)
+
+if (!univ_param$include_hcc){
+  holdouts <- c(holdouts, high_card_cat)
+}
+
 if (!univ_param$include_kw_var){
   holdouts <- c(holdouts, kw_var)
 }
+
 ydata <- as.numeric(data_all_processed2$interest_level[is_train])-1
 xvar <- setdiff(names(data_all_processed2), c("interest_level", holdouts))
 xdata <- Matrix(as.matrix(data_all_processed2[is_train,xvar]), sparse = TRUE)
@@ -151,7 +170,7 @@ cv_data <- lapply(1:5, function(i){
   xdata_i <- data_all_processed
   xdata_i$interest_level[is_test2] <- NA
   xdata_i <- add_high_card_cat(xdata_i)
-  if (univ_param$add_noise){
+  if (univ_param$add_hcc_noise){
     xdata_i <- add_noise(xdata_i, is_train2, high_card_cat)
   }
   xdata_i <- make_binary(xdata_i, kw_var, 0)
